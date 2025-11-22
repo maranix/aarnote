cf4ec01325bd32454580ee8c618661a72d76053f
Raman Verma <ramanverma4183@gmail.com>
2025-11-22 13:34:23 +0530

    git commit -m "feat: initialize project with expo and pnpm"

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+// https://docs.expo.dev/guides/using-eslint/
+const { defineConfig } = require('eslint/config');
+const expoConfig = require('eslint-config-expo/flat');
+const prettierRecommended = require('eslint-plugin-prettier/recommended');
+
+module.exports = defineConfig([
+  expoConfig,
+  prettierRecommended,
+  {
+    ignores: ['dist/*'],
+  },
+]);
